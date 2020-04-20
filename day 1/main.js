adc765dfcc23bdcae01fdde90eca4c81cfb68ce9
OumaESS <57940173+OumaESS@users.noreply.github.com>
2020-04-20 23:08:45 +0000

    add my work

--- day 1/main.js
+++ day 1/main.js
@@ -1,3 +1,6 @@
+//Workshop 1
+
+//1
 let x = 5;
 let y = "34";
 console.log(x+y)
@@ -33,3 +36,68 @@ console.log( x + y);
 x = true;
 y = false;
 console.log( x + y);
+
+x = false;
+y = true;
+console.log( x + y);
+
+x = false;
+y = true;
+console.log( x - y);
+
+x = 3;
+y = 4;
+console.log( x - y);
+
+x = "Bob" ;
+y = "bill";
+console.log( x - y);
+
+//2
+console.log(5 >= 1); 
+console.log(0 === 1); 
+console.log(4 <= 1); 
+console.log(1 != 1 ); 
+console.log("A" > "B"); 
+console.log("B" < "C"); 
+console.log("a" > "A"); 
+console.log("b" < "A"); 
+console.log(true === false); 
+console.log(true != true); 
+
+//3
+console.log("hello! It's" + " rainy " + " " + "out");
+
+/////////////////////////////////////////////////
+
+//Workshop2
+
+//1
+let firstName="Oumaima";
+let lastName="ESSAFIR";
+console.log(firstName+lastName);
+
+//2
+console.log(firstName+" "+lastName);
+
+//3
+var a=34;
+var b=21;
+console.log(a+b);//la somme est 55
+
+var b=21;
+var a=2;
+console.log(a+b);//la somme est 23
+
+//4
+var c;
+console.log(c);
+
+////////////////////////////////////////////////////////
+
+//Workshop3
+
+//la réponse dans work3.html
+
+///////////////////////////////////////////////////////
+
