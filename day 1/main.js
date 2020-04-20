52dcbea79e0d99a8d131996769c3f11de1618885
OumaESS <57940173+OumaESS@users.noreply.github.com>
2020-04-20 17:11:01 +0000

    workshop1

---
+++ day 1/main.js
@@ -0,0 +1,35 @@
+let x = 5;
+let y = "34";
+console.log(x+y)
+
+ x = 5;
+ y ='4';
+console.log(x-y);
+
+ x = 10;
+ y = 5;
+console.log( x % y);
+
+x = 5;
+y = 10;
+console.log( x % y);
+
+x = "Java";
+y = "Script";
+console.log( x + y);
+
+x = " ";
+y = " ";
+console.log( x + y);
+
+x = " ";
+y = 0;
+console.log( x + y);
+
+x = true;
+y = true;
+console.log( x + y);
+
+x = true;
+y = false;
+console.log( x + y);
