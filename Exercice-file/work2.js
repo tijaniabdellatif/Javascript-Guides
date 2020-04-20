ee35b8c7a4d718003b5e0e1d504e80b733bc9512
fatima-ezzahra <fatimaezzahrazahid7@gmail.com>
2020-04-20 23:35:20 +0000

    Create work2.js

---
+++ Exercice-file/work2.js
@@ -0,0 +1,17 @@
+//work2
+
+let firstName="fatimaezzahra";
+let lastName="zahid";
+console.log(firstName+lastName);
+console.log(firstName+" "+lastName);
+
+var a=34;
+var b=21;
+console.log(a+b);//55
+
+var b=21;
+var a=2;
+console.log(a+b);//23
+
+var c;
+console.log(c);//undefined
