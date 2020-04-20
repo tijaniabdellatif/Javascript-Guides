a0a4162d1e073fe13cbec345dd9fb45eb4b9ee40
lasridouae <lasridouae1@gmail.com>
2020-04-20 22:40:03 +0000

    workshop num 4 completed

---
+++ Day1/Exercice-file/workshop4.js
@@ -0,0 +1,33 @@
+//Wokrshop 4 :
+
+let age = parseInt(prompt('how old are you ?'));
+ 
+
+switch(age)
+{
+  case age<18:
+ 
+  alert( `Sorry, i can't let you drive me because you are under age`)
+  break;
+             
+  case age=18:
+  
+  alert(  `yes you can drive mebut carefully`)
+  break;
+        
+   case age<=40:
+    
+     alert( `yes you can drive me for sure`)
+      break;
+
+     default:
+ 
+    alert(  `Sorry, i can't let you drive me`)
+        
+        
+
+    }
+
+
+
+
