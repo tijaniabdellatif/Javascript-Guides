b3e5065c3d62461fa82ff3250c0db6b82def5bf0
lasridouae <lasridouae1@gmail.com>
2020-04-20 22:39:35 +0000

    adding files and workshop num1 and 2

---
+++ Day1/Exercice-file/index.js
@@ -0,0 +1,85 @@
+//1. Evaluation
+
+console.log(eval(5 + "34"));
+
+console.log(eval(5 - "4"));
+
+console.log(eval(10 % 5));
+
+console.log(eval(3 - 4));
+
+console.log(eval(" " + 0));
+
+console.log(eval("Bob" - "bill"));
+
+console.log(eval(" " + " "));
+
+console.log(eval(true + true));
+
+console.log(eval(true + false));
+
+console.log(eval(false + true));
+
+console.log(eval(false - true));
+
+let x ="java";
+let y = "script"
+console.log(x+y);
+
+//2. Evaluate the comparisons
+
+console.log(eval(5 >= 1));
+
+console.log(eval(0 === 1));
+
+console.log(eval(4 <= 1));
+
+console.log(eval(1 != 1));
+
+console.log(eval("A" > "B"));
+
+console.log(eval("B" < "C"));
+
+console.log(eval("a" > "A"));
+
+console.log(eval("b" < "A"));
+
+console.log(eval(true === false));
+
+console.log(eval(true != true));
+
+//3. making the string into a sentence
+
+
+console.log("hello!"+"its"+"rainy"+"out");
+
+//Wokrshop 2 :
+//1,2:
+let firstName = "douae";
+let lastName = "lasri";
+
+console.log(firstName+lastName);
+
+//the result is douaelasri
+
+console.log(firstName+ " " +lastName);
+
+//the result is douae lasri
+
+//3:
+var a = 34;
+var b = 21;
+var c;
+a = 2;
+
+ console.log(a + b);
+
+ // thes answer of a+b is 23
+
+ console.log(c==a);
+ //false
+ console.log(c==b);
+ //false
+ console.log(c==c);
+ //true
+
--- README.md
+++ README.md
@@ -1,2 +1,92 @@
-# Javascript-Guides
-It's a bunch of Javascript guides
+JAVASCRIPT TYPES
+-----------------
+1. Number 
+2. String
+3. Boolean
+4. Undefined
+5. Null
+<!-- 6. Symbol (new in ECMAScript 6) -->
+7. Object
+
+JAVASCRIPT COMPARISONS
+-----------------
+!==
+===
+>=
+<=
+>
+<
+JAVASCRIPT VARIABLES
+-----------------
+var
+<!-- let (new in ECMAScript 6)-->  
+<!-- const (new in ECMAScript 6)-->
+
+JAVASCRIPT CONDITIONALS
+-----------------
+if
+else
+else if
+<!-- ternary operator -->
+<!-- switch -->
+
+JAVASCRIPT LOGICAL OPERATORS
+-----------------
+&&
+||
+!
+JAVASCRIPT FUNCTIONS
+-----------------
+var a = function name() {}
+function name() {}
+return
+<!-- () => (new in ECMAScript 6) -->
+
+JAVASCRIPT DATA STRUCTURES
+-----------------
+Array
+Object
+
+JAVASCRIPT LOOPING
+-----------------
+for
+while
+do 
+forEach (new in ECMAScript 5) 
+
+
+JAVASCRIPT KEYWORDS
+-----------------
+break
+case
+catch
+class
+const
+continue
+debugger
+default
+delete
+do
+else
+export
+extends
+finally
+for
+function
+if
+import
+in
+instanceof
+new
+return
+super
+switch
+this
+throw
+try
+typeof
+var
+void
+while
+with
+yield
