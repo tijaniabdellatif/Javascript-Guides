d60bd522aee49d931c67bc867645fc0652540639
amine-dope <aminesailla85@gmail.com>
2020-04-20 23:00:22 +0000

    irst version work shop 3

---
+++ test.js
@@ -0,0 +1,12 @@
+const firstNumber = prompt("entrer le premier number"),
+     secondNumber = prompt("entrer le duxieme number"),
+   x = Number(firstNumber), 
+   y = Number(secondNumber),
+   sum = x + y,
+   kisma = x / y,
+   tarh = x - y,
+   darb = x * y; 
+console.log(sum);
+console.log(kisma);
+console.log(tarh);
+console.log(darb);
