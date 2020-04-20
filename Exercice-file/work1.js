3af7dc786fced67c47883e8f566178a1964acab9
fatima-ezzahra <fatimaezzahrazahid7@gmail.com>
2020-04-20 23:35:16 +0000

    Create work1.js

---
+++ Exercice-file/work1.js
@@ -0,0 +1,36 @@
+//work1
+console.log(5 + "34" );//534
+console.log(5 - "4");//1
+console.log(10 % 5);//0
+console.log(5 % 10 );//5
+console.log("Java" + "Script");//JavaScript
+console.log(true + true);//2
+console.log(true + false);//1
+console.log(false + true);//1
+console.log(false - true),//-1
+console.log("Bob" - "bill");//NaN
+
+console.log(5 >= 1);//true
+console.log(0 === 1);//false
+console.log(4 <= 1);//false 
+console.log(1 != 1 );//false
+console.log("A" > "B" );//false
+console.log("B" < "C");//true
+console.log("a" > "A");//true
+console.log("b" < "A");//false
+console.log(true === false);//false
+console.log(true != true);
+
+
+console.log("hello"+" "+"It's"+" "+"rainy");
+
+
+
+
+
+
+
+
+
+
+
