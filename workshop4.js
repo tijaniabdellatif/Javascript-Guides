d012349b1a7c8a24247160ff481d2d7f4279b67c
amine-dope <aminesailla85@gmail.com>
2020-04-20 23:00:28 +0000

    Create workshop4.js

---
+++ workshop4.js
@@ -0,0 +1,13 @@
+const msg = prompt("entrer ton age :") //22
+      y = Number(msg);
+      
+      if(y < 18){
+          alert('sorry, i can let you drive me ');
+
+    
+
+   } else if (y ===18){
+
+   } alert('yes, i can let you drive me carfely_ ');
+
+ 
