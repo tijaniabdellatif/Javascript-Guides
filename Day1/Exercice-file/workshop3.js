efc40f91cf6eb08d4820545f66b4d92bdf39f95b
lasridouae <lasridouae1@gmail.com>
2020-04-20 22:39:50 +0000

    workshop num 3 completed

---
+++ Day1/Exercice-file/workshop3.js
@@ -0,0 +1,18 @@
+//Wokrshop 3 :
+
+
+let x=parseInt(prompt("Your first number"));
+let y=parseInt(prompt("Your second number"));
+let z =prompt("Type an operator(+;-;*;/)");
+
+let firstNumber=parseInt(x);
+
+let secondNumber=parseInt(y);
+
+alert(firstNumber+secondNumber);
+alert(firstNumber-secondNumber);
+alert(firstNumber*secondNumber);
+alert(firstNumber/secondNumber);
+      
+     
+    
