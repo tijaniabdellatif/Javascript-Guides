b78eb4a27c8d63cb0f52a038f0ee67a29cad3a76
Salma Belassiri <salmabelassiri@gmail.com>
2020-04-21 10:15:28 +0000

    workshop2

---
+++ Day1/wokrshop 2.js
@@ -0,0 +1,27 @@
+//1
+
+let firstName = "Salma";
+let lastName = "Belassiri";
+
+console.log(firstName+lastName);
+
+//2
+
+console.log(firstName+" "+lastName);
+
+//3
+
+var a=34;
+var b=21;
+console.log(a+b); // output 55
+
+
+var b=21;
+var a=2;
+console.log(a+b); // output 23
+
+
+//4
+
+var c; 
+console.log(c);
