d31c3eaa41ecf4158c8ddb515e96e8c30e7532ab
abdeladim <abdeladim.boukrim@gmail.com>
2020-04-21 02:33:11 +0000

    Workshop-2

---
+++ day-N1/Wokrshop2.js
@@ -0,0 +1,17 @@
+let firstName="abdeladim";
+let lastName="boukrim";
+console.log(firstName+lastName);
+console.log(firstName+" "+lastName);
+
+var a=34;
+var b=21;
+console.log(a+b);
+
+a = 2 ;
+
+console.log(a+b);
+
+
+var c = a + b ;
+
+console.log(c);
