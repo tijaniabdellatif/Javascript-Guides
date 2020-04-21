1cc7614e95b8e6c436e50979ba540b55298f3d0d
abdeladim <abdeladim.boukrim@gmail.com>
2020-04-21 02:31:35 +0000

    Workshop

---
+++ day-N1/Wokrshop1.js
@@ -0,0 +1,91 @@
+let firstNumber = prompt ('Enter your first number');
+
+
+
+
+//## Wokrshop 1
+
+let a = 5 + "34";
+console.log(a);
+
+let b = 5 - "4"
+console.log(b);
+
+let c = 10 % 5;
+console.log(c);
+
+let d = "Java" + "Script";
+console.log(d);
+
+let e = " " + " " ;
+console.log(e);
+
+let f = " " + 0 ;
+console.log(f);
+
+let j = true + true ;
+console.log(j);
+
+let h = true + false ;
+console.log(h);
+
+let l = false + true ;
+console.log(l);
+
+let y = false - true ;
+console.log(y);
+
+let t = 3 - 4 ;
+console.log(t);
+
+let p = "Bob" - "bill" ;
+console.log(p);
+
+
+
+ 
+
+
+//2. Evaluate the below comparisons:
+
+
+let ab = 5 >= 1 ;
+console.log(ab) ;
+
+let bc = 0 === 1 ;
+console.log(bc);
+
+let cd = 4 <= 1 ;
+console.log(cd) ;
+
+let de = 1 != 1 ;
+console.log(de);
+
+let ef = "A" > "B" ;
+console.log(ef);
+
+let fe = "B" < "C" ;
+console.log(fe);
+
+let jk = "a" > "A" ;
+console.log(jk);
+
+let ol = "b" < "A" ;
+console.log(ol);
+
+let uo = true === false ;
+console.log(uo);
+
+let om = true != true ;
+console.log(om);
+
+//--3- "hello! It's "rainy" out"
+
+var  he =  "hello!"  ;
+var it = "It's" ;
+var ra = "rainy" ;
+var ou = " out";
+
+console.log(he +" "+ it +" "+ ra +" "+ ou );
+
+
