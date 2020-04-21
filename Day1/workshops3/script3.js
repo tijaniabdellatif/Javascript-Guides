2868dabbee2b5dc8e021112a5639a14006e315c3
othmane-qr <othmane.qorqach@gmail.com>
2020-04-21 00:54:54 +0000

    workshop3

---
+++ Day1/workshops3/script3.js
@@ -0,0 +1,10 @@
+//workshop3
+
+var x=prompt("entre number 1");
+var y=prompt("entre number 2");
+var Num1=Number(x);
+var Num2=Number(y);
+alert(Num1+Num2);
+alert(Num1*Num2);
+alert(Num1-Num2);
+alert(Num1/Num2);
