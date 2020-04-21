bb46428863e6678179700a392dc295f6294a317a
“[taha]” <taha.esselmouni@gmail.com>
2020-04-21 18:19:48 +0000

    corection

---
+++ Chapter tow/ex 02/script.js
@@ -0,0 +1,29 @@
+// Workshop 2 :
+// A triangle is a geometric shape defined by these 2 elements :
+// the base and the the height.
+
+// we got a multiple type of Triangles :
+
+// the Goal of this workshop is to create a triangle and manipulate it as well with the following steps:
+
+// use prompt() to get values and console() to input values
+
+// 1- Create a function : Triangle(ab,ac,cb,h)
+// ab , ac , cb=base are the ribs of the triangle
+// and h is the height, this function return an array
+// contain [ab,ac,cd,h]
+
+// 2- Inside the function  Triangle decide which type of triangle we got :
+//   if(ab = ac) {triangle isocel}
+//   else if(ab=ac=cb) {triangle equi}
+//   else {normal triangle}
+
+// 3- Create a function calculArea which take as parameter the return value of Triangle function, and returns the Area 
+
+// 4- Create a function Calcul Perimeter which take as parameter the return value of Triangle function, and returns the Area 
+
+// 5- Bonus : 
+
+// Try to do the Exercice with callBack function : 
+
+
