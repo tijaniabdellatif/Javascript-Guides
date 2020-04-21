cf40ef55c0d9349a51682c2b061ea3007bc5e9c1
abdoumar <abdoumar@gmail.com>
2020-04-21 13:53:23 +0000

    add Exp of callback fuction

---
+++ workMaths.js
@@ -0,0 +1,20 @@
+// const Func = (callbackFunc) => {
+
+
+//     let value = 30;
+//     callbackFunc(value);
+
+// }
+// Func((value)=> {
+//     console.log(value);
+// })
+const ul = document.querySelector('.names');
+cont people = [
+'karim','abdou','salim'];
+ let html = ``;
+
+ people.forEach(element => {
+     html += 
+     <li style="color:red">${tt}</li>;
+ });
+ ul.innerHTML
