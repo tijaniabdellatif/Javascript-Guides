a8e5f69fcb3684e90be90d904396148ab14b6a41
Salma Belassiri <salmabelassiri@gmail.com>
2020-04-21 10:15:12 +0000

    workshop 1

---
+++ Day1/wokrshop 1.js
@@ -0,0 +1,31 @@
+/* 1 */
+
+console.log(5 + "34"); // output "534",
+console.log(5 - "4"); // output 1,
+console.log(10 % 5); // output 0,
+console.log(5 % 10); // output 5,
+console.log("Java" + "Script"); // output "JavaScript",
+console.log(" " + " "); // output "  ",
+console.log(5 % 10); // output " 0",
+console.log(true + true); // output 2,
+console.log(true + false); // output 1,
+console.log(false + true); // output 1,
+console.log(false - true ); // output -1,
+console.log( 3 - 4); // output -1,
+console.log("Bob" - "bill"); // output NaN,
+
+/* 2 */
+console.log(5 >= 1); // output true,
+console.log(0 === 1); // output false,
+console.log(4 <= 1); // output false,
+console.log(1 != 1 ); // output false,
+console.log("A" > "B"); // output false,
+console.log("B" < "C"); // output true,
+console.log("a" > "A"); // output true,
+console.log("b" < "A"); // output false,
+console.log(true === false); // output false,
+console.log(true != true); // output false,
+
+/* 3 */
+console.log("hello! It's" +  " rainy " + " "  + "out"); // output "hello! It's rainy  out"
+
