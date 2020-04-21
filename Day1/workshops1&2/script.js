2b3a669729a6b2c5ffbe18396dce862f804a857a
othmane-qr <othmane.qorqach@gmail.com>
2020-04-21 00:54:32 +0000

    workshops 1&2

---
+++ Day1/workshops1&2/script.js
@@ -0,0 +1,124 @@
+let x = 5;
+let y = "34";
+console.log(x+y)
+
+ x = 5;
+ y ='4';
+console.log(x-y);
+
+ x = 10;
+ y = 5;
+console.log( x % y);
+
+x = 5;
+y = 10;
+console.log( x % y);
+
+x = "Java";
+y = "Script";
+console.log( x + y);
+
+x = " ";
+y = " ";
+console.log( x + y);
+
+x = " ";
+y = 0;
+console.log( x + y);
+
+x = true;
+y = true;
+console.log( x + y);
+
+x = true;
+y = false;
+console.log( x + y);
+
+x =  false;
+y = true;
+console.log( x + y);
+
+x =  false;
+y = true;
+console.log( x - y);
+
+x =  3;
+y = 4;
+console.log( x - y);
+
+x = "Bob" ;
+y = "bill";
+console.log( x - y);
+
+//2. Evaluate the below comparisons:
+x = 5 ;
+y = 1;
+console.log( x >= y);
+
+x = 0 ;
+y = 1;
+console.log( x === y);
+
+x = 4 ;
+y = 1;
+console.log( x <= y);
+
+x = 1 ;
+y = 1;
+console.log( x != y);
+
+x = "A" ;
+y = "B";
+console.log( x > y);
+
+x = "B" ;
+y = "C";
+console.log( x < y);
+
+x = "a" ;
+y = "A";
+console.log( x > y);
+
+x = "b" ;
+y = "A";
+console.log( x < y);
+
+x = true ;
+y = false;
+console.log( x === y);
+
+x = true ;
+y = true;
+console.log( x != y);
+
+
+console.log("hello! It's" +  " rainy " + " "  + "out"); 
+
+// workshop 2
+
+let firstName="qorqach";
+let lastName="othmane";
+console.log(firstName+lastName);//qorqachothmane
+console.log(firstName+" "+lastName);// qorqach othmane
+
+var a=34;
+var b=21;
+console.log(a+b);//55
+
+var b=21;
+var a=2;
+console.log(a+b);//23
+
+var c;
+console.log(c); //undefined
+
+
+
+
+
+
+
+
+
+
+
