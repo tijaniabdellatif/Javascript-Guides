9de089a8346d330d890e3654f581f40654742b8a
ayoubessafi <57940108+ayoubessafi@users.noreply.github.com>
2020-04-21 04:20:43 +0000

    add 4 workshops

---
+++ Exercice-file/example/script.js
@@ -0,0 +1,168 @@
+//workshop 1
+let x = 5;
+let y = "34";
+console.log(x + y);
+
+x = 5;
+y = "4";
+console.log(x - y);
+
+x = 10;
+y = 5;
+console.log(x % y);
+
+x = "java";
+y = "Script";
+console.log(x + y);
+
+x = "";
+y = "";
+console.log(x + y);
+
+x = "";
+y = "0";
+console.log(x + y);
+
+x = true;
+y = true;
+console.log(x + y);
+
+x = true;
+y = false;
+console.log(x + y);
+
+x = false;
+y = true;
+console.log(x + y);
+
+x = false;
+y = true;
+console.log(x - y);
+
+x = 3;
+y = 4;
+console.log(x - y);
+
+x = "Bob";
+y = "bill";
+console.log(x - y);
+
+x = 5;
+y = 1;
+console.log(x >= y);
+
+x = 0;
+y = 1;
+console.log(x === y);
+
+x = 4;
+y = 1;
+console.log(x <= y);
+
+x = 1;
+y = 1;
+console.log(x != y);
+
+x = "A";
+y = "B";
+console.log(x > y);
+
+x = "B";
+y = "C";
+console.log(x < y);
+
+x = "a";
+y = "A";
+console.log(x > y);
+
+x = "b";
+y = "A";
+console.log(x < y);
+
+x = "true";
+y = "false";
+console.log(x === y);
+
+x = "true";
+y = "true";
+console.log(x != y);
+
+console.log("hello! It's" + "rainy" + " " + "out");
+
+//workshop 2
+
+let firstName = "Ayoub";
+let lastName = "ESSAFI";
+console.log(firstName + lastName);
+
+firstName = "Ayoub";
+lastName = "ESSAFI";
+console.log(firstName + " " + lastName);
+
+var a = 34;
+var b = 21;
+console.log(a + b);
+
+a = 2;
+console.log(a + b);
+
+var c ;
+console.log(c);
+
+//workshop 3;
+
+// let num = prompt("enter first number");
+// let num2 = prompt("enter second number");
+
+// let v,w;
+// v = Number(num);
+// w = Number(num2);
+// alert(v + w);
+// alert(v * w);
+// alert(v - w);
+// alert(v / w);
+
+//workshop 4
+
+const conseil = prompt("enter your age");
+age = Number(conseil);
+// if(age < 18){
+//     alert(`Sorry, i can't let you drive`);
+// }
+// else if(age = 18){
+//     alert(`I can let you drive, but take easy for the car`);
+// }
+// else{
+//     alert(`I can let you drive`);
+// }
+
+//switch case
+
+switch(age)
+{
+    case age<18:
+    
+     alert(`Sorry, i can't let you drive`)
+     break;
+
+    case age=18:
+     
+     alert(`yes you can drive, but take easy for the car`)
+     break;
+
+    default:
+
+     alert(`i can let you drive`)
+
+
+}
+
+
+
+
+
+
+
+
+
+
