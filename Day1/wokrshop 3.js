9702ac5dfb8971b6b630245724e6c50e7b482539
Salma Belassiri <salmabelassiri@gmail.com>
2020-04-21 10:15:36 +0000

    workshop 3

---
+++ Day1/wokrshop 3.js
@@ -0,0 +1,18 @@
+
+
+let x =prompt("Enter first number");
+
+let first=parseFloat(x);
+
+
+let y=prompt("Enter second number");
+
+let second=parseFloat(y);
+
+
+
+alert(first+second);
+alert(first*second);
+alert(first-second);
+alert(first/second);
+
