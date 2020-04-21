eb923bd28e000f7758ec433620c6ce56bae80e82
amine-dope <aminesailla85@gmail.com>
2020-04-21 20:10:38 +0000

    modified

---
+++ day1/workshop4.js
@@ -0,0 +1,30 @@
+const msg = prompt("entrer ton age :") 
+      y = Number(msg);
+      
+// if(y < 18){
+//     alert('sorry, i can let you drive me ');
+// } else if (y ===18){
+//   alert(`yes, i can let you drive me carfely`);
+//  }else{
+//      alert(`yes, i can let you drive me`);
+//  }
+ 
+
+
+  switch (x) {
+      case x < 18:  // if(x < 18){
+          alert(`Sorry, i can let you drive me`)
+          break;
+  
+      case x === 18:
+          alert(`Yes, i can let you drive me carefully`)
+          break;
+  
+      default:
+          alert(`i can let you drive me`)
+          break;
+  }
+
+   
+
+ 
