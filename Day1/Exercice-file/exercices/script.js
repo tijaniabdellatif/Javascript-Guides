cbfdea7aa931b244a70115c8b678f574e9bbcb73
abdellmum <karimovich75@gmail.com>
2020-04-21 01:39:24 +0000

    Update script.js
    next workshop done

--- Day1/Exercice-file/exercices/script.js
+++ Day1/Exercice-file/exercices/script.js
@@ -1,16 +1,16 @@
-// ## Workshop 2 :
-// 1
-// var firstName="karim";
-// var lastName="nouaouri";
-// var fulName=firstName+" "+lastName;
-// console.log("my full name is :"+" "+fulName);//resultat est : my full name is abdelkarim nouaouri
-// // 2
-// var a = 34;
-// var b = 21;
-// a = 2; 
-// console.log(a+b);//variable est redéclaré avec une nouvelle valeur qui 2 alors a+b est 23(21+2)
-// var c; 
-// console.log(c);///undefined
+## Workshop 2 :
+1
+var firstName="karim";
+var lastName="nouaouri";
+var fulName=firstName+" "+lastName;
+console.log("my full name is :"+" "+fulName);//resultat est : my full name is abdelkarim nouaouri
+// 2
+var a = 34;
+var b = 21;
+a = 2; 
+console.log(a+b);//variable est redéclaré avec une nouvelle valeur qui 2 alors a+b est 23(21+2)
+var c; 
+console.log(c);///undefined
 var firstNumber = parseFloat(prompt("Enter the first number", ""));
 var secondNumber = parseFloat(prompt("Enter the second number", ""));
 var firstAnswer=firstNumber;
@@ -25,3 +25,15 @@ alert(firstAnswer + "*" + secondAnswer + "=" + mul);
 alert(firstAnswer + "/" + secondAnswer + "=" + div);
 
 
+// workshop 4
+
+var age = parseFloat(prompt("Enter your age please", ""));
+
+var userAge=age;
+
+if (userAge<=18){
+    alert("Sorry, i can let you drive me")
+}
+else{
+    alert("ok, i can let you drive me")
+}
