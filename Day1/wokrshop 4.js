3b564a55cfb65a7fa291cbac9bcfc1248d9e0dfe
Salma Belassiri <salmabelassiri@gmail.com>
2020-04-21 10:15:55 +0000

    workshop 4

---
+++ Day1/wokrshop 4.js
@@ -0,0 +1,14 @@
+var age=prompt("enter your age");
+
+if(age<18)
+ {
+         alert("Sorry, i can't let you drive me");
+ }
+ else if(age>=18 && age<=40)
+  {
+     alert(" i can let you drive me");
+}
+else
+ {
+     alert("Sorry, i can't let you drive me");
+ }
