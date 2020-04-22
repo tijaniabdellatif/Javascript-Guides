4313e988f02c44def2a6f63c3bf812fce9ec5305
amine-dope <aminesailla85@gmail.com>
2020-04-22 20:06:08 +0000

    create solution wok2

---
+++ day 2/work2.js
@@ -0,0 +1,24 @@
+function triangle(ab,ac,cb,h) {
+
+    ab=Number(prompt('entrer la valeur de ab'));
+    ac=Number(prompt('entrer la valeur de ac'));
+    cb=Number(prompt('entrer la valeur de la base'));
+    h=Number(prompt ('entrer la valeur de hauteur de triangle'));
+
+  const trg = [ ab,ac,cb,h]
+
+if(ab == ac) 
+{
+    console.log(`triangle isocel`);
+}
+else if(ab==ac && ac==cb)
+{
+    console.log(`triangle equi`);
+}
+else
+{
+    console.log(`normal triangle`);
+}
+return trg ;
+}
+triangle ();
