e298243e5656b4b5d3c86f4db587806b04ee44ca
fatima-ezzahra <fatimaezzahrazahid7@gmail.com>
2020-04-22 23:25:00 +0000

    solution work1

---
+++ Exercice-file/Day3/work1.js
@@ -0,0 +1,23 @@
+let getMony={
+    
+    people:[
+        { name: "amine",  age: 21,  budget: 15000 },
+        { name: "hamid",  age: 32, budget: 40000 },
+        { name: "soufiane",  age: 16, budget: 3500 }
+    ],
+    SumMony:function(name)
+    {
+        const fect=this.people.reduce((acc,curr)=>{
+
+            if(curr.name==name)
+            {
+                acc+=curr.budget;
+            }
+            return acc;
+        },0)
+        
+        return fect;
+       
+    }
+};
+console.log(getMony.SumMony("amine"));
