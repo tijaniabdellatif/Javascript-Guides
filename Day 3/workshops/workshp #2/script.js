7fa3c3e377a0666d09b5456462f992a2c105dd5d
osaelh <oussamaelh@gmail.com>
2020-04-22 21:00:10 +0000

    workshop #2

---
+++ Day 3/workshops/workshp #2/script.js
@@ -0,0 +1,32 @@
+let clients =[
+    {
+        name:'oussama',
+        total:1000
+    },
+    {
+        name:'otman',
+        total:3000
+    },
+    {
+        name:'ayoub',
+        total:2500
+    },
+    {
+        name:'ahmed',
+        total:10000
+    }
+]
+
+console.log(clients)
+
+const freeShipping = clients.map((item)=>{
+    if(item.total > 2500){
+        return {name : item.name,shipping:'free shipping'}
+    }
+    else {
+        return {name : item.name,shipping:'pay for shipping'}
+    }
+});
+
+
+console.log(freeShipping);
