d2b3d994dec279294f1638ef713f31c246e7b8c4
lasridouae <lasridouae1@gmail.com>
2020-04-22 22:19:28 +0000

    correction du workshop 2 day 2  ps: completed

--- Day 2/Workshop-File/workshop2.js
+++ Day 2/Workshop-File/workshop2.js
@@ -1,25 +1,25 @@
-function Triangle (ab,ac,cb,h){
+ function Triangle (){
 
    ab = Number(prompt('enter the (ab) value '));
    ac = Number(prompt('enter the (ac) value  '));
    cb= Number(prompt('enter the Triangle base '));
    h = Number(prompt('enter the Triangle height  '));
 
-    const Triangle = [ab,ac,cd,h];
+    
 
     if(ab == ac) {
 
-      console.log(`triangle isocel`);
+      console.log('triangle isocel');
     
        }
-    else if(ab==ac==cb) {
+    else if(ab == ac == cb) {
 
-       console.log(`triangle equi`);
+       console.log('triangle equi');
         
        }
     else {
 
-       console.log(`normal triangle`);
+       console.log('normal triangle');
       
        }
 
@@ -27,3 +27,12 @@ function Triangle (ab,ac,cb,h){
   
 }
 Triangle(); 
+function calculTriangleArea (base, height){
+
+ 
+  area =  ((base * height) /2)
+  console.log(`la surface de traingle est : ${area}`);
+   return area
+   
+}
+calculTriangleArea(val1, val2);
