4ccd5cd15ebc3b0dcccf59cffb55b94941b8ce89
fatima-ezzahra <fatimaezzahrazahid7@gmail.com>
2020-04-23 01:51:31 +0000

    solution work2

---
+++ Exercice-file/Day3/work2.js
@@ -0,0 +1,20 @@
+function Sumfre(arr)
+{
+   
+     var sum=0;
+      for(const i in arr)
+      {
+          sum+=arr[i];
+      }
+      if(sum>50)
+      {
+         console.log(`you are free to shop because your sum ${sum}`);
+      }
+      else
+      {
+       console.log(`you are not free to shop because your sum ${sum}`);
+      }
+   
+
+}
+Sumfre([5,6,1])
