6d346a94d409b2f18885cf76fe61bff8fd762e63
fatima-ezzahra <fatimaezzahrazahid7@gmail.com>
2020-04-23 16:20:29 +0000

    solution work 4

---
+++ Exercice-file/Day3/work4.js
@@ -0,0 +1,19 @@
+let person={
+    name:'amin',
+    notes:[5,6,3,20],
+    
+};
+function Affich()
+    {
+       var maxValue=Math.max(...person.notes);
+       return maxValue;
+     
+    }
+var topnote=Affich();
+function retu()
+{
+    person.topNote=topnote;
+    delete person.notes;
+    console.log(person);
+}
+retu();
