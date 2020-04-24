5913a21f7b85ea221b2847e517307664b04f8fab
MinaDakiri <minadakiri7@gmail.com>
2020-04-24 12:04:26 +0000

    workshop2 Day3

--- Day 3/Exercice-file/workshop2/script.js
+++ Day 3/Exercice-file/workshop2/script.js
@@ -1,1 +1,22 @@
 
+//## workshop 2 :
+
+
+  function isEligible(arr){
+    var summ = 0;
+    for(var i in arr){
+        summ += arr[i];
+    }
+    return summ;
+}
+
+var arr = new Array(11.99, 35.99, 41.99);
+var result = isEligible(arr);
+console.log(result);
+if (result > 50) {
+        console.log('you are free to shop');
+    }
+    
+    else{
+        console.log('you are not');
+    }
