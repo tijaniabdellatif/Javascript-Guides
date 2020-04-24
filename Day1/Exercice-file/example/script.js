96a9986b816be2e737b3e3c6774dd03fc0cc8ff6
cheymae <58041410+chiamae@users.noreply.github.com>
2020-04-24 00:41:32 +0000

    Update script.js

---
+++ Day1/Exercice-file/example/script.js
@@ -0,0 +1,60 @@
+workshop2:
+let firstName = " Cheymae ";
+let lastName = " Raoudi ";
+console.journal(prénom + nom);
+console.log(firstName + "  " + lastName);
+
+var a = 34;
+var b = 21;
+console.log(a + b);
+
+a = 2;
+
+console.log(a + b);
+
+var c = a + b;
+
+console.log(c);
+Question1
+console.log(5 + " 34 "); // sortie "534",
+console.journal(5 - " 4 "); // sortie 1,
+console.journal(10 % 5); // sortie 0,
+console.journal(5 % 10); // sortie 5,
+console.log(" Java " + " Script "); // sortie "JavaScript",
+console.log("  " + "  "); // sortie "",
+console.journal(5 % 10); // sortie "0",
+console.log(vrai + vrai); // sortie 2,
+console.log(vrai + faux); // sortie 1,
+console.log(faux + vrai); // sortie 1,
+console.log(faux - vrai); // sortie -1,
+console.log(trois - quatre); // sortie -1,
+console.journal(" Bob " - " facture "); // sortie NaN,
+
+Question2
+console.log(5 > = 1); // sortie vrai,
+console.journal(0 === 1); // sortie false,
+console.log(4 <= 1); // sortie false,
+console.log(1! = 1); // sortie false,
+console.log(" A " > " B "); // sortie false,
+console.log(" B " < " C "); // sortie vrai,
+console.log(" a " > " A "); // sortie vrai,
+console.log(" b " < " A "); // sortie false,
+console.log(vrai === faux); // sortie false,
+console.log(vrai! = vrai); // sortie false,
+
+Question3
+console.log(" bonjour! C'est " + " pluvieux " + "  " + " out "); // sortie "bonjour! il pleut"
+workchops: 3
+laissez x = invite(" Entrez le premier numéro ");
+
+let first = parseFloat(x);
+
+
+laissez y = prompt(" Entrez le deuxième nombre ");
+
+soit second = parseFloat(y);
+
+alerte(première + seconde);
+alerte(première * seconde);
+alerte(première - seconde);
+alerte(première / seconde);
