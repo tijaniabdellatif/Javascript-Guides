2f29ebcddd4db86106b08edec044f2ab01a12e98
lasridouae <lasridouae1@gmail.com>
2020-04-24 00:08:40 +0000

    worksho numb 4 day 3 done

---
+++ Day 3/Exercice-file/workshop4.js
@@ -0,0 +1,15 @@
+
+let array = [
+    { name: "amine", notes: [3, 5, 4]}
+  ];
+
+  let topnote = 5;
+
+function retur()
+{
+    array.name = "amine";
+    array.topNote=topnote;
+    console.log(array);
+}
+retur();
+         
