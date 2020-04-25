d95050e26fc300e10628559224e441fefee004ef
osaelh <oussamaelh@gmail.com>
2020-04-25 22:05:56 +0000

    update (arrow function)

--- Day 3/workshops/workshp #2/script.js
+++ Day 3/workshops/workshp #2/script.js
@@ -19,14 +19,18 @@ let clients =[
 
 console.log(clients)
 
-const freeShipping = clients.map((item)=>{
-    if(item.total > 2500){
-        return {name : item.name,shipping:'free shipping'}
-    }
-    else {
-        return {name : item.name,shipping:'pay for shipping'}
-    }
-});
+// const freeShipping = clients.map((item)=>{
+//     if(item.total > 2500){
+//         return {name : item.name,shipping:'free shipping'}
+//     }
+//     else {
+//         return {name : item.name,shipping:'pay for shipping'}
+//     }
+// });
+
+//using arrow function
+
+const freeShipping  = clients.map(item => (item.total >= 2500 ? {name :item.name,shipping :'free shipping'} : {name : item.name, shipping : 'pay for shipping'}));
 
 
 console.log(freeShipping);
