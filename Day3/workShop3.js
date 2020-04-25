7a26602d862b6508ffe2377cfb1ca131dbfd6e5d
Ziko <Ziko@gmail.com>
2020-04-25 01:07:27 +0000

    finish workShop3 Day3

--- Day3/workShop3.js
+++ Day3/workShop3.js
@@ -1,10 +1,12 @@
 const free = () => {
-    const product = { "fati": 19, "zack": 15, "achraf": 30, "ouma": 76, "ayoub": 26 };
+    const product = { "fati": 19, "zack": 15, "achraf": 30, "ouma": 13, "ayoub": 26 };
     const values = Object.values(product);
-    console.log(values);
+    // console.log(values);
     for(i = 0 ; i < values.length; i++){
         if(values[i] <18){
-            console.log(`the teenager person Among them is ${values[i]}`)
+            const stk = values.indexOf(values[i]);
+            // console.log(stk);
+            console.log(`the teenager person Among them is ${Object.keys(product)[stk]}`)
         }else{
             -1
         } 
