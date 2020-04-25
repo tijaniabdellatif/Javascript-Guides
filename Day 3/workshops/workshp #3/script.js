37ecfcf12de602928b72b67a844a92c1601e8ee6
osaelh <oussamaelh@gmail.com>
2020-04-25 21:35:40 +0000

    update

--- Day 3/workshops/workshp #3/script.js
+++ Day 3/workshops/workshp #3/script.js
@@ -5,17 +5,20 @@ let people = {
     "amin": 15,
    "karim": 4}
             
-function getMinor(people){
-  const arr = [],
-  minors = Object.keys(people);
-  for (var x in minors){
-    if(people[minors[x]] < 18 ){
-      arr.push(minors[x]);
-    }
-  }
-  return arr;
-}
+// function getMinor(people){
+//   const arr = [],
+//   minors = Object.keys(people);
+//   for (var x in minors){
+//     if(people[minors[x]] < 18 ){
+//       arr.push(minors[x]);
+//     }
+//   }
+//   return arr;
+// }
 
-console.log(getMinor(people))
+// console.log(getMinor(people))
+
+const getMinor = people.filter(age => 18)
+console.log(getMinor)
 
 // console.log(Object.getOwnPropertyNames(people).filter(prop => veg[prop] > 5));
