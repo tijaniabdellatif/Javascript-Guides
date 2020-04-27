2cc520118199c88e0273345f7c0bde4b92aae90f
othmane-qr <othmane.qorqach@gmail.com>
2020-04-27 02:43:54 +0000

    add work2 day 3

---
+++ day3/workshops2/script.js
@@ -0,0 +1,18 @@
+function Eligible(arr){
+    var some = 0;
+    for(var i in arr){
+        some += arr[i];
+    }
+    return some;
+}
+
+var arr = new Array(11.99, 5.99, 41.99);
+var result = Eligible(arr);
+console.log(result);
+if (result > 50) {
+        console.log('you are free to shop');
+    }else{
+        console.log('you are  not');
+    }
+
+
