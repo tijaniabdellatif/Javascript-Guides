4947e51d12852f27eebe20f6a45394965fb0d607
yahyabouhlal <yahya.bouhlal.b@gmail.com>
2020-04-27 15:27:32 +0000

    modiff

--- Day3/script.js
+++ Day3/script.js
@@ -7,10 +7,7 @@ const group = () => {
         { name: "soufiane", age: 16, budget: 7500 },
     ];
     let some = 0;
-    // for (let i = 0 ; i <= getMoney.length; i++) {
-    //     some += getMoney[i].budget;
-    //     console.log(some);
-    // }
+   
     getMoney.forEach(element => {
         if(element.name){
             some += element.budget;
