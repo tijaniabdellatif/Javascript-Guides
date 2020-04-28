f9b47f751f791ef32dae6dd4c0c27e2b7fb4e2a6
abdoumar <abdoumar@gmail.com>
2020-04-28 16:55:32 +0000

    add same Ex

---
+++ workshopDAY3/P_Money.js
@@ -0,0 +1,13 @@
+
+//{name:"abdou",
+//age : 23, budget : 4000},
+//{name:"ahmed",
+//age : 28, budget : 4000},
+//{name:"ali",
+//age : 56, budget : 4000},
+
+getMoney([
+    { name: "abdou", age: 23, budget: 4000 },
+    { name: "ahmed",  age: 28, budget: 40000 },
+    { name: "yassin",  age: 15, budget: 3500 }])
+ 
